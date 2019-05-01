67ede1cb9385460cb779b5c6470d5d29b80c394a
Weijia Wang <weijiaw3@student.unimelb.edu.au>
2019-05-01 17:49:34 +1000

    initial commit
    Project initial set up

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    Vector3 lookingDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ray = new Ray();
+        lookingDirection = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookingDirection = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, lookingDirection, out hit))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.green);
+        }
+    }
+}
